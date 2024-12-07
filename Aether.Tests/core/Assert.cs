5d9fbeb0b950e1d82fcc2b59dede34c60af835b6
theryo12 <theryo12@gmail.com>
2024-12-07 21:36:26 +0300

    combined 2 methods into one

--- Aether.Tests/core/Assert.cs
+++ Aether.Tests/core/Assert.cs
@@ -1,37 +1,9 @@
 namespace Aether.Tests.Core;
 
 // TODO: docs
-// make areequal, istrue one method
 public static class Assert
 {
-    public static void AreEqual<T>(T expected, T actual, string? message = null) where T : IEquatable<T>
-    {
-        if (!expected.Equals(actual))
-        {
-            throw new AssertException(
-                message ?? $"Assertion Failed: Expected <{expected}>, but got <{actual}>."
-            );
-        }
-    }
-
-    public static void AreEqual<T>(ReadOnlySpan<T> expected, ReadOnlySpan<T> actual, string? message = null)
-    where T : IEquatable<T>
-    {
-        if (expected.Length != actual.Length)
-        {
-            throw new AssertException(message ?? $"Assertion Failed: Array lengths differ. Expected {expected.Length}, but got {actual.Length}.");
-        }
-
-        for (int i = 0; i < expected.Length; i++)
-        {
-            if (!expected[i].Equals(actual[i]))
-            {
-                throw new AssertException(message ?? $"Assertion Failed: Arrays differ at index {i}. Expected <{expected[i]}>, but got <{actual[i]}>.");
-            }
-        }
-    }
-
-    public static void IsTrue(bool condition, string? message = null)
+    public static void That(bool condition, string? message = null)
     {
         if (!condition)
         {
